b438384f40f99edcd6c76d8bdba81e439e1de023
AbYsMeL <melvinjlobo@users.noreply.github.com>
2024-11-14 13:58:35 -0800

    Time: 6 ms (5.56%) | Memory: 42.7 MB (24.98%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -29,7 +29,21 @@ class Solution {
 
     // Binary exponentiation...Same concept as above
     public double pow1(double x, int pow) {
+        /**
+        We have to convert the exponent to long since it does not handle neagtive numbers like -
+        pow = -2147483648
+        To handle cases where the exponent n is negative, we:
+
+         Convert n to a long so we can safely take the absolute value (this prevents overflow for cases like n = Integer.MIN_VALUE).
+        Then, we perform the binary exponentiation process on this positive exponent.
+
+        E.g
         long n = pow;
+
+        Other alternative is to use >>> instead of >> since >> preserves the sign bits...>> just fills the left with zeroes
+         */
+        long n = pow;
+
         //Normalize Negative Case
         if(n < 0) {
             x = 1/x;
@@ -46,7 +60,7 @@ class Solution {
 
             x *= x;             //Multiply by itself for each bit shift
             
-            n >>= 1;            //Shift the bit (equivalen to divide n by 2)
+            n >>>= 1;            //Shift the bit (equivalen to divide n by 2)
         }
 
         return result;
