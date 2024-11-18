709794c56ce33daf0dee417eebb6496a45a371e3
AbYsMeL <melvinjlobo@users.noreply.github.com>
2024-11-17 23:22:06 -0800

    Time: 0 ms (100.00%) | Memory: 41.9 MB (9.20%) - LeetSync

--- 921-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 921-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,4 +1,24 @@
+/**
+
+To solve this problem:
+
+1. Use a counter to track unbalanced parentheses:
+    A.Open Count (openCount): Keeps track of unmatched opening parentheses '('.
+    B. Close Count (closeCount): Keeps track of unmatched closing parentheses ')'.
+2. Traverse the string:
+    A. If you encounter '(', increment openCount.
+    B. If you encounter ')':
+        I. If openCount > 0, it means there's a matching opening parenthesis, so 
+        decrement openCount.
+        II. Otherwise, increment closeCount because this ')' is unmatched.
+3. At the end of the traversal:
+    openCount will represent unmatched '('.
+    closeCount will represent unmatched ')'.
+    Hence, The result is openCount + closeCount.
+ */
+
 class Solution {
+    
     public int minAddToMakeValid(String s) {
         int openCount = 0;
         int closeCount = 0;
