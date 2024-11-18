f51af2b8829b6db3d43c6b3bf81195b118b91878
AbYsMeL <melvinjlobo@users.noreply.github.com>
2024-11-17 23:11:46 -0800

    Time: 46 ms (18.13%) | Memory: 59.5 MB (63.18%) - LeetSync

--- 1762-buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
+++ 1762-buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
@@ -1,3 +1,12 @@
+/**
+Traverse through the list of building heights. 
+The idea is to push the bldgs in the stack. Ocen view to right means that all bldgs
+in the stack should be in descending order from bottom to top...
+In other words if the current building being processed is taller than the building at the top of the stack we need to pop since it will block the ocean view of the bldg at top of stack (as it is taller in height)
+If stack is empty or the current building is shorter than the top of the stack push the building onto the stack.
+At the end the stack will contain an ascending list of the buildings that have a view of the ocean.
+ */
+
 class Solution {
     public int[] findBuildings(int[] heights) {
         Stack<Integer> stack = new Stack<>();
