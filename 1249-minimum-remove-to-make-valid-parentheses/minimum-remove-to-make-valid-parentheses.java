712471c272dcb12cbe8cd8dbbc75f48115c42d05
AbYsMeL <melvinjlobo@users.noreply.github.com>
2024-11-30 23:07:11 -0800

    Time: 8 ms (97.89%) | Memory: 45.4 MB (49.88%) - LeetSync

--- 1249-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,5 +1,59 @@
 class Solution {
+
+    /**
+        The general idea is to keep a count of open paranthesis. Whenever we encounter:
+        1. "(": we increment the counter
+        2. ")":
+            A. If the counter is zero, all "(" have been balanced. So this ")" is unbalanced. Mark for deletion
+            B. If the counter is > zero, decrement it since this ")" balances an "("
+        
+        3. Once the loop is done, we have taken care of all ")", but if the counter is zero, we still have to take care of "("
+        4. Run a loop from the end of the string and if we encounter "(", mark it for deletion and reduce the counter. 
+        Why from the end?: This is because, we have taken care of all the ")". This means that all the "(" in the counter are AFTER we are done balancing them with ")"...which means they are towards the RHS...as all the ones from 0th index have been taken care of. Let's look at a few cases:
+        1. **(*((**)**): Here after balancing, we see that the 3rd "(" is unbalanced...whici it towards the RHS
+        2. **(**(***))**(**: Here also, we see that the extra "(" is towards teh RHS
+        3. (************: Same as above
+
+     */
     public String minRemoveToMakeValid(String s) {
+        int openCounter = 0;
+        char open = '(', close = ')';
+        char[] arr = s.toCharArray();
+
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i] == open)
+                openCounter++;
+            else if(arr[i] == close) {
+                if(openCounter == 0)
+                    arr[i] = '9';
+                else
+                    openCounter--;
+            } else
+                continue;
+            
+        }
+
+        //Now that we have balanced all braces with ")", we check of any remaining "("
+        for(int i = arr.length - 1; i >= 0 && openCounter > 0; i--) {
+            if(arr[i] == open) {
+                arr[i] = '9';
+                openCounter--;
+            }
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] != '9')
+                sb.append(arr[i]);
+        }
+
+        return sb.toString();
+    }
+
+
+    public String minRemoveToMakeValidWithStack(String s) {
         
         Stack<Pair<Character, Integer>> paranStack = new Stack<>();
         StringBuilder result = new StringBuilder();
