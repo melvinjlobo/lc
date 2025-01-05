4819e614bdd99e4f250462205187d9f69e9312af
AbYsMeL <melvinjlobo@users.noreply.github.com>
2025-01-05 00:07:00 -0800

    Time: 7 ms (7.36%) | Memory: 58.3 MB (12.57%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,9 +1,15 @@
 /**
 We take two pointers, one at the beginning and one at the end of the array constituting the length of the lines. Further, we maintain a variable maxarea to store the maximum area obtained till now. At every step, we find out the area formed between them, update maxarea, and move the pointer pointing to the shorter line towards the other end by one step.
+
+Time complexity: O(n). Single pass.
+
+Space complexity: O(1). Constant space is used.
+
  */
 
 class Solution {
     public int maxArea(int[] height) {
+        
         int left = 0, right = height.length - 1;
         int maxArea = 0;
 
