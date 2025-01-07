b29efb9c84249bc6b4c3d067b7b1b78f6599f55f
AbYsMeL <melvinjlobo@users.noreply.github.com>
2025-01-06 16:22:10 -0800

    Time: 2 ms (83.44%) | Memory: 44.7 MB (55.17%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,33 @@
+/**
+    https://takeuforward.org/data-structure/combination-sum-1/
+ */
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        helper(candidates, target, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    // Recursive call helper
+    private void helper(int[] candidates, int target, int index, List<Integer> currCombo, List<List<Integer>> result) {
+        // Base case
+        if(index == candidates.length) {
+            if(target == 0) {
+                result.add(new ArrayList<>(currCombo));
+            }
+            return;
+        }
+
+        // Here we don't need to run the whole loop. We run two recursions one with and one without.
+        // The below is for with (interested in index)
+        if(candidates[index] <= target) {
+            currCombo.add(candidates[index]);
+            helper(candidates, target - candidates[index], index, currCombo, result);
+            currCombo.remove(currCombo.size() - 1);
+        }
+
+        // This is for without (Not interested in the index). So we move on to the next Index
+        helper(candidates, target, index + 1, currCombo, result);
+    }
+}
