040bbd06fd9cc891a9d246d3ea67cfd648672d3f
AbYsMeL <melvinjlobo@users.noreply.github.com>
2025-02-03 22:44:47 -0800

    Time: 2 ms (70.92%) | Memory: 42.9 MB (21.84%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -15,7 +15,7 @@ class Solution {
         int freshOranges = 0;
         int m = grid.length, n = grid[0].length;
         Queue<Pair> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[m][n];
+        //boolean[][] visited = new boolean[m][n];
         int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
         int time = 0, rottenCount = 0, minutes = 0;
 
@@ -24,7 +24,7 @@ class Solution {
             for (int j = 0; j < n; j++) {
                 if(grid[i][j] == 2){
                     queue.offer(new Pair(i, j));
-                    visited[i][j] = true;
+                    //visited[i][j] = true;
                 }
                 else if (grid[i][j] == 1)
                     freshOranges++;
@@ -44,9 +44,9 @@ class Solution {
                 for(int[] dir : dirs) {
                     int newRow = cell.row + dir[0];
                     int newCol = cell.col + dir[1];
-                    if (canRot(grid, m, n, visited, newRow, newCol)) {
+                    if (canRot(grid, m, n, newRow, newCol)) {
                         grid[newRow][newCol] = 2;
-                        visited[newRow][newCol] = true;
+                        //visited[newRow][newCol] = true;
                         queue.offer(new Pair(newRow, newCol));
                         freshOranges--;
                         hasRotten = true;
@@ -61,8 +61,8 @@ class Solution {
         return freshOranges == 0 ? minutes :  -1;
     }
 
-    public boolean canRot(int[][] grid, int m, int n, boolean[][] visited, int row, int col) {
-        if(row >= 0 && row < m && col >= 0 && col < n && grid[row][col] == 1 && !visited[row][col])
+    public boolean canRot(int[][] grid, int m, int n, int row, int col) {
+        if(row >= 0 && row < m && col >= 0 && col < n && grid[row][col] == 1)
             return true;
         return false;
        
