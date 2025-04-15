e8718a44c4ef96bb7a1c7b23701b409bd6b92b24
AbYsMeL <melvinjlobo@users.noreply.github.com>
2025-04-14 19:57:18 -0700

    Time: 0 ms (100.00%) | Memory: 42.3 MB (73.26%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,3 +1,16 @@
+/**
+    One of the key elements to understand is that beyond both the left and right borders is "-infinity" as per the problem, so this looks like: "-inf [] -inf".
+    The means that:
+    1. If the first element is greater than the second element (and it obviously is greater than - infinity), so it is one of the peaks and we don't need to search the array
+    2. If the last element is greater than the penultimate, it's obviously greater than the -inf outside the array bounds, so it is the peak element and we don't need to search the array further
+    3. For any other element, we run a loop from 1 to n-2 (since we already handled the cases on the edges of the array) and we check:
+        I. If the mid element is greater than either sides, then it is the peak
+        II. else if it is on an increasing slope peaking towards the left (mid element greater than the one on right), remove the right half
+        III. else we are on the increasing slope peaking towards the right, remove the left half
+
+ */
+
+
 class Solution {
     // public int findPeakElement(int[] nums) {
 
